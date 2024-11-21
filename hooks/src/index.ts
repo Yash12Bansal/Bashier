28829e63f749c3baf27c146b60f47fe08fd9abe8
Yash12Bansal <NasaYash1>
2024-11-21 14:55:58 +0530

    added processor and worker

---
+++ hooks/src/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import { PrismaClient} from '@prisma/client';
+const client=new PrismaClient();
+const app= express();
+
+app.post("/hooks/catch/:userId/:zapId", async(req, res)=>{
+    const userId=req.params.userId
+    const zapId=req.params.zapId;
+    const body=req.body;
+
+    // store in db a new trigger
+    await client.$transaction(async tx=>{
+        const run = await tx.zapRun.create({
+            data:{
+                zapId:zapId,
+                metadata:body
+            }
+        });
+    
+        await tx.zapRunOutbox.create({
+            data:{
+                zapRunId:run.id,
+            }
+        })
+    })
+    res.json({
+        msg:"Webhook reveived"
+    })
+    // push it in the a kafka/redis queue    
+});
+
+
+app.listen(3000);
